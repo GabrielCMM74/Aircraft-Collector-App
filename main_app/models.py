0c17c53aac0333b61fd8608382f5d61143ab684a
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-04-25 10:00:08 -0400

    Part 2 lab done

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
+from sys import builtin_module_names, dont_write_bytecode
 from django.db import models
 
 # Create your models here.
+class Aircraft(models.Model):
+    name = models.CharField(max_length=50)
+    manufacturer = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    developedInto = models.TextField(max_length=50)
+    dob = models.TextField(max_length=50)
+    built = models.IntegerField()
+    # new code below
+    def __str__(self):
+        return self.name
+
+
+
+
+# a = Aircraft(name="Lockheed Martin F-22 Raptor", manufacturer="Lockheed Martin Aeuronautics | Boeing Defense | Space & Security", description="The aircraft is equipped to operate autonomously in combat over hostile territory, in escort of deep-penetration strike aircraft and in the suppression of enemy airfields.", developedInto="Lockheed Martin X-44 MANTA", dob="September 7, 1997", built=195)
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from .models import Aircraft
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('aircrafts/', views.aircrafts_index, name='index'),
+    path('aircrafts/<int:aircraft_id>/', views.aircrafts_detail, name='detail'),
 ]
